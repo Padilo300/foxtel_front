d20c467b0d2e10ac70618dc129e46ae9d9996782
padilo300 <padilo300@gmail.com>
2019-06-25 16:45:28 +0300

    главная без футера

--- app/js/my.js
+++ app/js/my.js
@@ -1,30 +1,32 @@
 $(document).ready(function () {
 
+    function openNav(){
+        $('.mobilBar').toggleClass("openNav");
+        $('#nav').toggleClass('showNav');
+    }
+
+    $('.mobilBar').on('click',function(){
+        openNav();
+    });
     
-    /////////////////////////////////////////////////////////
-    /////                                               /////
-    ///// Запуск счетчика цифр при скролле              /////   
-    /////                                               /////
-    /////////////////////////////////////////////////////////
-    
-    var show = true;
-    var countbox = "#counts";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false;                      // Отменяем показ анимации, если она уже была выполнена
-        var w_top       = $(window).scrollTop()     ; // Количество пикселей на которое была прокручена страница
-        var e_top       = $(countbox).offset().top  ; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height    = $(window).height()        ; // Высота окна браузера
-        var d_height    = $(document).height()      ; // Высота всего документа
-        var e_height    = $(countbox).outerHeight() ; // Высота всего документа
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $(".benefits__number").spincrement();
-            // $('.benefits__number').css('opacity', '1');
-            // $('.benefits__number').spincrement({
-            //     thousandSeparator: "",
-            //     duration: 1200
-            // });
-            show = false;
-        }
+    $('#mainPageMainSection-slick').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.main_section .prevArrow'),
+        nextArrow: $('.main_section .nextArrow'),
+        dotsClass: 'mainPageMainSection-slickDots',
     });
 
+    $('#pageHomeSliderReview').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.pageHomeSliderReview .prevArrow'),
+        nextArrow: $('.pageHomeSliderReview .nextArrow'),
+    });
 });
