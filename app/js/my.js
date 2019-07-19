274ddd3db5f5e60d2b8aa6584614eb4d7c4196b9
padilo300 <padilo300@gmail.com>
2019-07-19 10:09:53 +0300

    верстка готова

--- app/js/my.js
+++ app/js/my.js
@@ -109,7 +109,22 @@ $(document).ready(function () {
         $(this).siblings(".listNav").toggleClass('active');
     });
 
-
+    $(document).on('click', '.number-spinner button', function () {    
+        var btn = $(this),
+            oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+            newVal = 0;
+        
+        if (btn.attr('data-dir') == 'up') {
+            newVal = parseInt(oldValue) + 1;
+        } else {
+            if (oldValue > 1) {
+                newVal = parseInt(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        btn.closest('.number-spinner').find('input').val(newVal);
+    });
 
    
      new WOW().init();
